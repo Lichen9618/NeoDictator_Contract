e6bdd71d52dba735a9a0577679d2498b7ac8f8b6
晨 黎 <lichen@neo.org>
2021-09-09 17:08:46 +0800

    add core contract check

--- NeoDictator_Contract/voteContract/Vote.cs
+++ NeoDictator_Contract/voteContract/Vote.cs
@@ -16,9 +16,15 @@ namespace VoteContract
     [ManifestExtra("Description", "This is a VoteContract")]
     public class HelloContract : SmartContract
     {
+        //TODO;部署前确认coreContract Hash
+        [InitialValue("0x5ba6c543c5a86a85e9ab3f028a4ad849b924fab9", ContractParameterType.Hash160)]
+        private static readonly byte[] coreContract = default;
+
+        public static event Action<object> Notify;
         public static BigInteger ClaimGasByCore()
         {
             //验证是否为Core合约
+            Require(Runtime.CheckWitness((UInt160)coreContract), "check core witness fail");
             BigInteger gasBalanceBefore = GAS.BalanceOf(Runtime.ExecutingScriptHash);
             Contract.Call(NEO.Hash, "transfer", CallFlags.All, Runtime.ExecutingScriptHash, Runtime.ExecutingScriptHash, NEO.BalanceOf(Runtime.ExecutingScriptHash));
             BigInteger gasBalanceAfter = GAS.BalanceOf(Runtime.ExecutingScriptHash);
@@ -28,6 +34,7 @@ namespace VoteContract
         public static bool UnstakeByCore(UInt160 toAddress, BigInteger amount, object data) 
         {
             //验证是否为core合约
+            Require(Runtime.CheckWitness((UInt160)coreContract), "check core witness fail");
             return (bool)Contract.Call(NEO.Hash, "transfer", CallFlags.All, Runtime.ExecutingScriptHash, toAddress, amount);            
         }
 
@@ -40,14 +47,24 @@ namespace VoteContract
         public static bool ChangeNEOPosition(UInt160 target, BigInteger amount) 
         {
             //验证是否为core合约
+            Require(Runtime.CheckWitness((UInt160)coreContract), "check core witness fail");
             return (bool)Contract.Call(NEO.Hash, "transfer", CallFlags.All, Runtime.ExecutingScriptHash, target, amount);
         }
 
         public static bool ChangeVoteTargetByCore(Neo.Cryptography.ECC.ECPoint voteTo) 
         {
             //验证core合约
+            Require(Runtime.CheckWitness((UInt160)coreContract), "check core witness fail");
             NEO.Vote(Runtime.ExecutingScriptHash, voteTo);
             return true;
+        }
+        private static void Require(bool condition, string info)
+        {
+            if (condition == false)
+            {
+                Notify(info);
+                throw new Exception();
+            }
         }
     }
 }
